936d17935bf00b89b5e40a4f31a7ffd1f3460b09
ViniVanC <vincentvan0302@gmail.com>
2023-03-19 21:11:23 +0200

    added colors to tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        darkGreen: "#212721",
+        greyGreen: "#4A524A",
+        yellow: "#FFEA00",
+        white: "#D6D1CA",
+        grey: "rgba(214, 209, 202, 0.7)",
+      },
       fontFamily: {
         PlayFair: ["Playfair Display", "serif"],
         SourceSans: ["Source Sans 3", "sans-serif"],
