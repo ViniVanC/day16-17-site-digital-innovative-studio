a8647968d0b71db60283d610bea71ee5331e1d14
ViniVanC <vincentvan0302@gmail.com>
2023-03-20 21:23:01 +0200

    bug-fix

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ import useMediaQuery from "../hooks/useMediaQuery";
 
 const Title = ({ children }) => {
   return (
-    <div className="max-[533px]:text text-[173px] uppercase leading-[100%] max-md:text-[130px] max-sm:mb-[30px] max-sm:text-[100px] max-sm:leading-[120%]">
+    <div className="max-[533px]:text text-[173px] uppercase leading-[100%] max-md:text-[130px] max-sm:mb-[30px] max-sm:text-[100px] max-sm:leading-[120%] max-[550px]:text-[50px]">
       {children}
     </div>
   );
